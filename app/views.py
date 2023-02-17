0282e809d280bc8ed407a63eac97cf5d3715b673
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-17 17:24:34 +0530

    admin created

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from django.views.generic.edit import CreateView
+from .models import Student
 # Create your views here.
+class CreateStudent(CreateView):
+    model=Student
+    template_name='app/home.html'
+    # label={'email':'emailadd'}
+    fields=['name','email','password']
