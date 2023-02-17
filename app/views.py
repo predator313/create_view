d6b1f8256c6f65ff2b6e9bb701351d9ea94771fd
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-17 19:27:30 +0530

    all the urls is working fine

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Student
+from django.views.generic.base import TemplateView
 # Create your views here.
 class CreateStudent(CreateView):
     model=Student
     template_name='app/home.html'
     fields=['name','email','password']
-    success_url='/'
+    success_url='/thank/'
+
+class ThankView(TemplateView):
+    template_name='app/thanku.html'
