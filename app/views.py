f324dad1ea5b0b60ec04f7e747537b5d023f8129
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-17 20:05:16 +0530

    using method get_form to gives the class to ours form

--- app/views.py
+++ app/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render,HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Student
 from django.views.generic.base import TemplateView
+from django import forms
 # Create your views here.
 class CreateStudent(CreateView):
     model=Student
     template_name='app/home.html'
     fields=['name','email','password']
     success_url='/thank/'
+    #now use the method get_from 
+    #to give class to the particular label
+    def get_form(self):
+        form= super().get_form()
+        form.fields['name'].widget=forms.TextInput(attrs={'class':'myclass'})
+        form.fields['password'].widget=forms.PasswordInput(attrs={'class':'pass'})
+        return form
 
 class ThankView(TemplateView):
     template_name='app/thanku.html'
